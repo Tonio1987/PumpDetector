69f6dae7da0737874d452bf1fea7ebae89a42e82
Anthony AVOLA <anthony.avola@gmail.com>
2020-02-28 11:27:55 +0100

    Edit notifier - use only price qlert

--- persistence/backend/notifier/DB_EvolCtypto.js
+++ persistence/backend/notifier/DB_EvolCtypto.js
@@ -15,17 +15,19 @@ module.exports = {
             WHERE EVC_LAST_INSERT = 1
             AND
             (
-                EVC_EVOL_VOLUME_ON_PERIOD_STATUS = 'ALERT'
-                OR EVC_EVOL_VOLUME_ON_PERIOD_STATUS = 'WARNING'
+                //EVC_EVOL_VOLUME_ON_PERIOD_STATUS = 'ALERT'
+                //OR EVC_EVOL_VOLUME_ON_PERIOD_STATUS = 'WARNING'
                 OR EVC_EVOL_PRICE_ON_PERIOD_STATUS = 'ALERT'
                 OR EVC_EVOL_PRICE_ON_PERIOD_STATUS = 'WARNING'
-                OR EVC_EVOL_NB_TRADES_ON_PERIOD_STATUS = 'ALERT'
-                OR EVC_EVOL_NB_TRADES_ON_PERIOD_STATUS = 'WARNING'
+                //OR EVC_EVOL_NB_TRADES_ON_PERIOD_STATUS = 'ALERT'
+                //OR EVC_EVOL_NB_TRADES_ON_PERIOD_STATUS = 'WARNING'
             )
              */
 
-            let sql = 'SELECT * FROM T_ALGO_EVOL_CRYPTO_EVC WHERE EVC_LAST_INSERT = 1 AND RIGHT(EVC_SYMBOL, 3) = ? AND (EVC_EVOL_VOLUME_ON_PERIOD_STATUS = ? OR EVC_EVOL_VOLUME_ON_PERIOD_STATUS = ? OR EVC_EVOL_PRICE_ON_PERIOD_STATUS = ? OR EVC_EVOL_PRICE_ON_PERIOD_STATUS = ? OR EVC_EVOL_NB_TRADES_ON_PERIOD_STATUS = ? OR EVC_EVOL_NB_TRADES_ON_PERIOD_STATUS = ?) ORDER BY EVC_EVOL_PRICE_ON_PERIOD DESC';
-            db.connection.query(sql, [btc, alert, warning, alert, warning, alert, warning], function (err, result) {
+            //let sql = 'SELECT * FROM T_ALGO_EVOL_CRYPTO_EVC WHERE EVC_LAST_INSERT = 1 AND RIGHT(EVC_SYMBOL, 3) = ? AND (EVC_EVOL_VOLUME_ON_PERIOD_STATUS = ? OR EVC_EVOL_VOLUME_ON_PERIOD_STATUS = ? OR EVC_EVOL_PRICE_ON_PERIOD_STATUS = ? OR EVC_EVOL_PRICE_ON_PERIOD_STATUS = ? OR EVC_EVOL_NB_TRADES_ON_PERIOD_STATUS = ? OR EVC_EVOL_NB_TRADES_ON_PERIOD_STATUS = ?) ORDER BY EVC_EVOL_PRICE_ON_PERIOD DESC';
+            let sql = 'SELECT * FROM T_ALGO_EVOL_CRYPTO_EVC WHERE EVC_LAST_INSERT = 1 AND RIGHT(EVC_SYMBOL, 3) = ? AND (EVC_EVOL_PRICE_ON_PERIOD_STATUS = ? OR EVC_EVOL_PRICE_ON_PERIOD_STATUS = ?) ORDER BY EVC_EVOL_PRICE_ON_PERIOD DESC';
+            //db.connection.query(sql, [btc, alert, warning, alert, warning, alert, warning], function (err, result) {
+            db.connection.query(sql, [btc, alert, warning], function (err, result) {
                 if (err) {
                     reject(err);
                 }
